f6de4b0df0a81302a60e20745ca6ae0cca5c4ba6
Teacher-droid <teaching.know@gmail.com>
2022-06-09 12:20:56 +0200

    data location

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ Bundler.require
 require_relative './routers/router.rb'
 
 # Defines the data location
+$csv = "./data/shop.csv"
 
 # Launches the whole program logic by invoking the perform method on the newly created Router object
 Router.new.perform
