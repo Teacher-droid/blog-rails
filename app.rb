9ca652448be6f4d3cef2803f7e364fc167a868ca
Teacher A <teaching.know@gmail.com>
2022-07-03 22:25:41 +0200

    adding variable

--- app.rb
+++ app.rb
@@ -4,10 +4,13 @@ require 'bundler'
 Bundler.require
 require_relative './routers/router.rb'
 
-# Defines the data location
-$csv = "./data/shop.csv"
+# global variable
+$csv = './data/shop.csv'
+$verbose = true
+$shop_name = 'ToTo\' Lair'
+$admin_pwd = '1234'   # Would obviously gain to be encrypted and stored in / accessed from a '.env' file or variable in a future version
 
-# Launches the whole program logic by invoking the perform method on the newly created Router object
+# Launches the whole program logic
 Router.new.perform
 
 
